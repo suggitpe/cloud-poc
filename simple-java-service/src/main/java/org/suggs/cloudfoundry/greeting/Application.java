b762db830d4c916705d562067f7a159a00536587
Peter Suggitt <me@suggs.org.uk>
2016-02-02 07:38:54 +0000

    updated simple servive to only be a service

--- simple-java-service/src/main/java/org/suggs/cloudfoundry/greeting/Application.java
+++ simple-java-service/src/main/java/org/suggs/cloudfoundry/greeting/Application.java
@@ -2,9 +2,10 @@ package org.suggs.cloudfoundry.greeting;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

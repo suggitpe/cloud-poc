6202370593ced9aa0a12f8e3b8ee643f246e6f49
Peter Suggitt <me@suggs.org.uk>
2016-02-06 10:13:56 +0000

    added a consumer

---
+++ simple-java-service-consumer/src/main/java/org/suggs/cloudfoundry/consumer/ConsumerController.java
@@ -0,0 +1,22 @@
+package org.suggs.cloudfoundry.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/services/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+        return discoveryClient.getInstances(applicationName);
+    }
+}

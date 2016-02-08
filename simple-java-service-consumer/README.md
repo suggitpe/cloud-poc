4488eb06095cc7768a409d9d68e7cb34b48b66f7
Peter Suggitt <me@suggs.org.uk>
2016-02-08 10:33:22 +0000

    Created a service consumer

---
+++ simple-java-service-consumer/README.md
@@ -0,0 +1,4 @@
+
+
+* [Simple Java Service](http://localhost:8902/services/simple-java-service)
+* [Simple Java Service Controller](http://localhost:8902/services/simple-java-service-controller)
--- simple-java-service-consumer/src/main/java/org/suggs/cloudfoundry/consumer/Application.java
+++ simple-java-service-consumer/src/main/java/org/suggs/cloudfoundry/consumer/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)

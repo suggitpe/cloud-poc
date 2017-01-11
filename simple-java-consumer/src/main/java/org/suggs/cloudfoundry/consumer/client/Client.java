bb7ad4aebd784ea4fa804a2df391ef5ba1d4c440
Peter Suggitt <me@suggs.org.uk>
2017-01-11 14:46:44 +0000

    updated dockerfiles etc

--- simple-java-consumer/src/main/java/org/suggs/cloudfoundry/consumer/client/Client.java
+++ simple-java-consumer/src/main/java/org/suggs/cloudfoundry/consumer/client/Client.java
@@ -1,6 +1,6 @@
 package org.suggs.cloudfoundry.consumer.client;
 
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Client {
--- simple-java-consumer/src/main/java/org/suggs/cloudfoundry/consumer/greeting/Greeting.java
+++ simple-java-consumer/src/main/java/org/suggs/cloudfoundry/consumer/greeting/Greeting.java
@@ -1,6 +1,6 @@
 package org.suggs.cloudfoundry.consumer.greeting;
 
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Greeting {

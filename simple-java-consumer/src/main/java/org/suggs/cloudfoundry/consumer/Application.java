466ce716ede375187809ac6f9f1cc2cebcf24b4b
Peter Suggitt <me@suggs.org.uk>
2017-01-18 15:34:19 +0000

    updated restTeomplate so that its load balanced with Eureka

--- simple-java-consumer/src/main/java/org/suggs/cloudfoundry/consumer/Application.java
+++ simple-java-consumer/src/main/java/org/suggs/cloudfoundry/consumer/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +17,7 @@ public class Application {
                 .run(args);
     }
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder aBuilder){
         return aBuilder.build();

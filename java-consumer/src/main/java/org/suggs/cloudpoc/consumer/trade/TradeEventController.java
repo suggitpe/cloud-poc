31a90681f12d39b55943074e8d640d24abd153d3
Peter Suggitt <me@suggs.org.uk>
2018-08-15 08:47:35 +0100

    trade event view implemented

--- java-consumer/src/main/java/org/suggs/cloudpoc/consumer/trade/TradeEventController.java
+++ java-consumer/src/main/java/org/suggs/cloudpoc/consumer/trade/TradeEventController.java
@@ -1,15 +1,16 @@
 package org.suggs.cloudpoc.consumer.trade;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@RestController
+@Controller
 public class TradeEventController {
 
     @Autowired
@@ -21,6 +22,15 @@ public class TradeEventController {
     @ResponseBody
     public TradeEvent getTradeDataAsJson(@PathVariable String domain, @PathVariable String id, @PathVariable Long version) {
         return executeTradeEventRestCallFor(domain, id, version);
+    }
+
+    @RequestMapping(value = "/tradeEvent/{domain}/{id}/{version}", method = GET)
+    public String getTradeEventAsWebPage(@PathVariable String domain, @PathVariable String id, @PathVariable Long version, Model model){
+        model.addAttribute("domain", domain);
+        model.addAttribute("id", id);
+        model.addAttribute("version", version);
+        model.addAttribute("data", executeTradeEventRestCallFor(domain, id, version));
+        return "tradeEventDataView";
     }
 
     private TradeEvent executeTradeEventRestCallFor(String tradeDomain, String tradeId, Long tradeVersion) {

47c9390a8747d014fc863f9792f2e6ddd1b6fbaf
Peter Suggitt <me@suggs.org.uk>
2023-11-20 20:34:40 +0000

    updated build for disco service

--- discovery-service/src/main/java/org/suggs/cloudpoc/discovery/DiscoveryService.java
+++ discovery-service/src/main/java/org/suggs/cloudpoc/discovery/DiscoveryService.java
@@ -1,21 +1,13 @@
 package org.suggs.cloudpoc.discovery;
 
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static org.springframework.boot.WebApplicationType.SERVLET;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableSwagger2
 public class DiscoveryService {
     public static void main(String[] args) {
         new SpringApplicationBuilder(DiscoveryService.class)
@@ -23,13 +15,5 @@ public class DiscoveryService {
                 .run(args);
     }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
 
